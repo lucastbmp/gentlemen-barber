2c95c99c7db5b89cd531c5b1e456d56fcd3ffb4c
Lucas Tobias <lucasmarinari7@gmail.com>
2025-05-12 22:57:50 -0300

    feat(navbar): box-shadow, feat(home): add background image, color-gradient, main text

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/home/index.jsx'
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <Home />
--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navbar from "../../components/navbar"
- 
+import Chair from "../../assets/images/chair.png" 
+import './index.css'
 export default function Home() {
+
 return (
     <>
-        <Navbar />
-        <main>
-            <h1>Home</h1>
-        </main>
+       	<Navbar />
+	<div className="main-rapper">
+	    
+	<div className="chair-bg"><img src={Chair} /></div>
+            <h1>Lorem ipsum</h1>
+        </div>
     </>
 )
 
