a56e03c3df7920b926ef0c83bde4f7bac80b0825
lucasmarinari <lucasmarinari7@gmail.com>
2025-05-13 14:37:03 -0300

    style(text): add text style, style(chair-bg): add bottom gradient, add top and left margin, TODO: add react-router-dom.

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -10,7 +10,13 @@ return (
 	<div className="main-rapper">
 	    
 	<div className="chair-bg"><img src={Chair} /></div>
-            <h1>Lorem ipsum</h1>
+            <div className="text">
+	        <h1>
+	            Lorem ipsum dolor sit amet
+	        </h1>
+	        <h2>Consectetur adipiscing elit. Pellentesque non nulla leo. Curabitur mollis rutrum ligula at interdum. Vivamus eget mauris nec justo tincidunt maximus. In aliquet interdum sapien, a commodo lorem. Quisque aliquet ipsum eget suscipit iaculis. Pellentesque placerat lorem id viverra rhoncus. Sed nec pulvinar urna. Nam vel gravida ante. Proin vulputate ac ex non vestibulum. Donec nulla justo, imperdiet in dignissim et, accumsan ut magna.
+	        </h2>
+            </div>
         </div>
     </>
 )
