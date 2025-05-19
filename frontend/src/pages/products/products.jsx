3ef39b2d402c6a2e975876a01166d21c71760a7a
Lucas Tobias <lucasmarinari7@gmail.com>
2025-05-18 21:05:56 -0300

    style(products): add carousel to desktop, fix hover bug, add click on item to change slide

--- frontend/src/pages/products/products.jsx
+++ frontend/src/pages/products/products.jsx
@@ -1,63 +1,65 @@
-    import React from 'react';
-    import Product from '../../components/products-card/products-card';
-    import './style.css';
-    import "slick-carousel/slick/slick.css";
-    import "slick-carousel/slick/slick-theme.css";
-    import Slider from "react-slick";
+import React from 'react';
+import Product from '../../components/products-card/products-card';
+import './style.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
-    const productArr = [
-    { productName: 'Loção', price: '60' },
-    { productName: 'Shampoo', price: '60' },
-    { productName: 'Óleo', price: '60' },
-    { productName: 'Shampoo Fresh Men', price: '60' },
-    { productName: 'Pomada Modeladora', price: '60' },
-    { productName: 'Gel', price: '60' },
-    { productName: 'Balm', price: '60' },
-    ];
+const productArr = [
+  { productName: 'Loção', price: '60' },
+  { productName: 'Shampoo', price: '60' },
+  { productName: 'Óleo', price: '60' },
+  { productName: 'Shampoo Fresh Men', price: '60' },
+  { productName: 'Pomada Modeladora', price: '60' },
+  { productName: 'Gel', price: '60' },
+  { productName: 'Balm', price: '60' },
+];
 
-    export default function Products() {
-    const isMobile = window.matchMedia('(max-width: 768px)').matches;
-
-    const settings = {
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 1,
-        infinite: false,
-        speed: 500,
-        arrows: true,
-        dots: false,
-        responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-            centerPadding: '40px',
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-            centerPadding: '20px',
-            arrows: false,
-            }
+export default function Products() {
+  const settings = {
+    centerMode: true,
+    centerPadding: '190px',
+    slidesToShow: 1,
+    arrows: false,
+    dots: false,
+    infinite: false,
+    speed: 500,
+    cssEase: 'ease-in-out',
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          centerPadding: '60px',
+          arrows: true
         }
-        ]
-    };
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: '30px',
+          slidesToShow: 1,
+          arrows: true,
+        }
+      }
+    ]
+  };
 
-    return (
-        <section className="products-section" id="products">
-        <div className="product-content">
-            <h1>Nossos produtos</h1>
-            <Slider className="products-carousel" {...settings}>
-            {productArr.map((produto) => (
-                <div key={produto.productName} className="carousel-slide">
-                <Product
-                    productName={produto.productName}
-                    price={produto.price}
-                />
-                </div>
-            ))}
-            </Slider>
-        </div>
-        </section>
-    );
-    }
+  return (
+    <section className="products-section" id="products">
+      <div className="product-content">
+        <h1>Nossos produtos</h1>
+        <Slider className="products-carousel" {...settings}>
+          {productArr.map((produto) => (
+            <div key={produto.productName} className="carousel-slide">
+              <Product
+                productName={produto.productName}
+                price={produto.price}
+              />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </section>
+  );
+}
